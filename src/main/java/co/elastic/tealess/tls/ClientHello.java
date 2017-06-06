7657d4898a861a6b4acbfd937dadf25eb47a38cb
Jordan Sissel <jls@semicomplete.com>
2017-06-06 11:20:18 -0700

    Add more TLS decoding code.

--- src/main/java/co/elastic/tealess/tls/ClientHello.java
+++ src/main/java/co/elastic/tealess/tls/ClientHello.java
@@ -9,11 +9,11 @@ public class ClientHello extends TLSHandshake {
   private final Version version;
   private final Random random;
   private final byte[] session;
-  private final List<Short> cipherSuites;
+  private final List<CipherSuite> cipherSuites;
   private final List<Byte> compressionMethods;
   private final byte[] extensionData;
 
-  public ClientHello(Version version, Random random, byte[] session, List<Short> cipherSuites, List<Byte> compressionMethods, byte[] extensionData) {
+  public ClientHello(Version version, Random random, byte[] session, List<CipherSuite> cipherSuites, List<Byte> compressionMethods, byte[] extensionData) {
     super();
     this.version = version;
     this.random = random;
@@ -21,6 +21,10 @@ public class ClientHello extends TLSHandshake {
     this.cipherSuites = cipherSuites;
     this.compressionMethods = compressionMethods;
     this.extensionData = extensionData;
+  }
+
+  public String toString() {
+    return String.format("%s[%d cipher suites]", getClass().getSimpleName(), cipherSuites.size());
   }
 
 }
