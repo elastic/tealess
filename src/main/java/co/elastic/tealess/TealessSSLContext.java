af13fbd099ee3824f1fe1d280afce873f8bc4679
Jordan Sissel <jls@semicomplete.com>
2017-10-10 21:00:53 -0700

    Make TealessSSLContext public

--- src/main/java/co/elastic/tealess/TealessSSLContext.java
+++ src/main/java/co/elastic/tealess/TealessSSLContext.java
@@ -4,7 +4,7 @@ import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLContextSpi;
 import java.security.Provider;
 
-class TealessSSLContext extends SSLContext {
+public class TealessSSLContext extends SSLContext {
     // required as a subclass for SSLContext
     protected TealessSSLContext(SSLContextSpi sslContextSpi, Provider provider, String s) {
         super(sslContextSpi, provider, s);
