88c0e80fa175cde495a2ca0ab638a50a124b32fb
Jordan Sissel <jls@semicomplete.com>
2017-10-10 21:07:09 -0700

    Add static build method. I think TealessSSLContext has to be a protected constructor.

--- src/main/java/co/elastic/tealess/TealessSSLContext.java
+++ src/main/java/co/elastic/tealess/TealessSSLContext.java
@@ -6,7 +6,11 @@ import java.security.Provider;
 
 public class TealessSSLContext extends SSLContext {
     // required as a subclass for SSLContext
-    public TealessSSLContext(SSLContextSpi sslContextSpi, Provider provider, String s) {
+    protected TealessSSLContext(SSLContextSpi sslContextSpi, Provider provider, String s) {
         super(sslContextSpi, provider, s);
+    }
+
+    public static SSLContext create(SSLContextSpi sslContextSpi, Provider provider, String s) {
+        return new TealessSSLContext(sslContextSpi, provider, s);
     }
 }
