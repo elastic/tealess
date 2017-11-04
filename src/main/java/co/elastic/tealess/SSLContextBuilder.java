ea053ca0fdb573a8bd3fe7d8a4cc2031fd17a553
Jordan Sissel <jls@semicomplete.com>
2017-11-03 23:40:43 -0700

    Work on actionable error messages when choosing cipher suites or protocols.
    Started adding tests for SSLContextBuilder for cipher suite use, including a special case where we know the corrective action is to install the Oracle JCE Unlimited Strength Cryptography policy.

--- src/main/java/co/elastic/tealess/SSLContextBuilder.java
+++ src/main/java/co/elastic/tealess/SSLContextBuilder.java
@@ -23,19 +23,46 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import javax.net.ssl.*;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
 import java.security.*;
 import java.security.cert.X509Certificate;
 import java.util.Arrays;
 
 public class SSLContextBuilder {
-  private final SecureRandom random = new SecureRandom();
-  private final String trustManagerAlgorithm = TrustManagerFactory.getDefaultAlgorithm();
-  private final Logger logger = LogManager.getLogger();
-  private KeyStore trustStore;
-  private SSLCertificateVerificationTracker tracker;
-  private KeyManagerFactory keyManagerFactory;
-  private String[] cipherSuites = null;
+  private static final String ORACLE_JVM_RUNTIME_NAME = "Java(TM) SE Runtime Environment";
 
+  private static final String trustManagerAlgorithm = TrustManagerFactory.getDefaultAlgorithm();
+  private static final Logger logger = LogManager.getLogger();
+
+  private static final SSLCertificateVerificationTracker defaultTracker = (chain, authType, exception) -> System.out.println("Server certificate chain: " + chain);
+  private static final SSLParameters defaultParameters;
+  private static final SSLParameters supportedParameters;
+
+  static {
+    final SSLContext defaultContext;
+    try {
+      defaultContext = SSLContext.getDefault();
+    } catch (NoSuchAlgorithmException e) {
+      // A java.lang.Error feels appropriate for this kind of catastrophic failure.
+      throw new Error("SSLContext.getDefault() failed. This means something is very wrong, and unexpected, with the JVM or its configuration.", e);
+    }
+
+    defaultParameters = defaultContext.getDefaultSSLParameters();
+    supportedParameters = defaultContext.getSupportedSSLParameters();
+  }
+
+  // XXX: I don't see a strong reason for someone to want to provide their own. Maybe during testing?
+  private final SecureRandom random = new SecureRandom();
+  private SSLCertificateVerificationTracker tracker = defaultTracker;
+  private String[] cipherSuites = defaultParameters.getCipherSuites();
+  private String[] protocols = defaultParameters.getProtocols();
+
+  private KeyStore trustStore;
+  private KeyManagerFactory keyManagerFactory;
+
+  // XXX: Do we need this method to be public?
   public void setTracker(SSLCertificateVerificationTracker tracker) {
     this.tracker = tracker;
   }
@@ -50,7 +77,13 @@ public class SSLContextBuilder {
 
   public SSLContext build() throws KeyManagementException, KeyStoreException, NoSuchAlgorithmException {
     SSLContext ctx = SSLContext.getInstance("TLS");
+
     KeyManager[] kms = null;
+
+    if (tracker == null) {
+      // XXX: We want a tracker per-socket so we can access this data during exception handling.
+      tracker = (chain, authType, exception) -> System.out.println("Server certificate chain: " + chain);
+    }
 
     if (keyManagerFactory != null) {
       kms = Arrays.stream(keyManagerFactory.getKeyManagers())
@@ -58,18 +91,9 @@ public class SSLContextBuilder {
         .toArray(X509KeyManager[]::new);
     }
 
-    if (tracker == null) {
-      // XXX: We want a tracker per-socket so we can access this data during exception handling.
-      tracker = (chain, authType, exception) -> System.out.println("Server certificate chain: " + chain);
-    }
-
     TrustManager[] tms = buildTrustStore();
 
     logger.trace("Building SSLContext with keys:{}, trusts:{}", kms, tms);
-
-    if (cipherSuites == null) {
-      cipherSuites = SSLContext.getDefault().getDefaultSSLParameters().getCipherSuites();
-    }
 
     SSLContextSpi spi = new TealessSSLContextSpi(ctx, cipherSuites);
     SSLContext tealessContext = new TealessSSLContext(spi, null, null);
@@ -78,8 +102,7 @@ public class SSLContextBuilder {
   }
 
   private TrustManager[] buildTrustStore() throws NoSuchAlgorithmException, KeyStoreException {
-    TrustManagerFactory tmf;
-    tmf = TrustManagerFactory.getInstance(trustManagerAlgorithm);
+    final TrustManagerFactory tmf = TrustManagerFactory.getInstance(trustManagerAlgorithm);
 
     if (trustStore != null) {
       logger.trace("Using custom trust store with " + trustStore.size() + " entries");
@@ -88,20 +111,82 @@ public class SSLContextBuilder {
       logger.trace("Using system default trust store");
       tmf.init((KeyStore) null);
     }
+
     // Wrap java's TrustManagers in our own so that we can track verification failures.
-    return Arrays.stream(tmf.getTrustManagers())
-            .map((tm) -> new TrackingTrustManager((X509TrustManager) tm, tracker))
-            .toArray(TrustManager[]::new);
+    //return Arrays.stream(tmf.getTrustManagers())
+            //.map((tm) -> new TrackingTrustManager((X509TrustManager) tm, tracker))
+            //.toArray(TrustManager[]::new);
+    return tmf.getTrustManagers();
   }
 
   public void setKeyManagerFactory(KeyManagerFactory keyManagerFactory) {
     this.keyManagerFactory = keyManagerFactory;
   }
 
-  public void setCipherSuites(String[] cipherSuites) {
+  public void setCipherSuites(String[] cipherSuites) throws IllegalArgumentException {
+    for (String cipherSuite : cipherSuites) {
+      if (!isValidCipherSuite(cipherSuite)) {
+        // XXX: do special handling if this cipher suite is known to be enabled by Oracle's JCE Unlimited Strength Encryption
+        // ^^^ It'd be nice to notify the user of the obvious remediation if we know this is unsupported because of Oracle's JCE USC.
+        // If the ciphersuite is known but probably disabled because the user is using Oracle JRE without JCE Unlimited Strength installed
+        try {
+          SSLContext c = SSLContext.getDefault();
+          SSLEngine sslEngine = c.createSSLEngine();
+          sslEngine.setEnabledCipherSuites(new String[]{"TLS_RSA_WITH_AES_256_CBC_SHA256"});
+        } catch (IllegalArgumentException e) {
+          // Wrap the IllegalArgumentException message in something that includes more context for the user.
+          if (e.getMessage().startsWith("Unsupported ciphersuite " + cipherSuite)) {
+            // probably an unknown or invalid cipher suite name.
+            throw new IllegalArgumentException(e.getMessage() + ". Supported cipher suites are: " + Arrays.asList(supportedParameters.getCipherSuites()));
+          } else if (e.getMessage().equals("Cannot support " + cipherSuite + " with currently installed providers")) {
+            // This likely means the user is asking for a cipher that is disabled by some Java configuration.
+            // In my testing, I found this occurred when using Oracle Java *without* the Unlimited Strength Cryptography policy installed.
+            // For example, the cipher suite "TLS_RSA_WITH_AES_256_CBC_SHA256" will fail with the above error if the JCE USC is not installed.
+            // This only impacts Oracle JVM, not OpenJDK.
+            // This check should only be needed for older Java 8 releases since Unlimited Strength is now default: https://bugs.openjdk.java.net/browse/JDK-8170157
+            if (System.getProperty("java.runtime.name").equals(ORACLE_JVM_RUNTIME_NAME)) {
+              throw new IllegalArgumentException(e.getMessage() + ". This probably means you need to install the Oracle Java Cryptography Extension Unlimited Strength Cryptographic Policy. Supported cipher suites are: " + Arrays.asList(supportedParameters.getCipherSuites()));
+            } else {
+              throw new IllegalArgumentException(e.getMessage() + ". Supported cipher suites are: " + Arrays.asList(supportedParameters.getCipherSuites()));
+            }
+          }
+        } catch (NoSuchAlgorithmException e) {
+          e.printStackTrace();
+        }
+
+        throw new IllegalArgumentException("The cipher suite '" + cipherSuite + "' is not supported. Supported cipher suites are: " + Arrays.asList(supportedParameters.getCipherSuites()));
+      }
+    }
     this.cipherSuites = cipherSuites;
   }
 
+  private boolean isValidCipherSuite(String cipherSuite) {
+    for (String suite : supportedParameters.getCipherSuites()) {
+      if (cipherSuite.equals(suite)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public void setProtocols(String[] protocols) {
+    for (String protocol : protocols) {
+      if (!isValidProtocol(protocol)) {
+        throw new IllegalArgumentException("The protocol '" + protocol + "' is not supported. Supported protocols are: " + Arrays.asList(supportedParameters.getCipherSuites()));
+      }
+    }
+
+    this.protocols = protocols;
+  }
+
+  private boolean isValidProtocol(String protocol) {
+    for (String supportedProtocol: supportedParameters.getProtocols()) {
+      if (protocol.equals(supportedProtocol)) {
+        return true;
+      }
+    }
+    return false;
+  }
 
   public interface SSLCertificateVerificationTracker {
     void track(X509Certificate[] chain, String authType, Throwable exception);
---
+++ src/test/java/co/elastic/tealess/SSLContextBuilderTest.java
@@ -0,0 +1,51 @@
+package co.elastic.tealess;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.Test;
+
+import javax.net.ssl.SSLContext;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.*;
+import static org.junit.Assume.assumeFalse;
+import static org.junit.Assume.assumeThat;
+
+public class SSLContextBuilderTest {
+  SSLContextBuilder builder = new SSLContextBuilder();
+
+  @Test(expected = IllegalArgumentException.class)
+  public void setCipherSuitesWithInvalidCipherSuite() throws Exception {
+    builder.setCipherSuites(new String[]{"foo"});
+  }
+
+  @Test
+  public void setCipherSuitesWithValidCipherSuite() throws Exception {
+  }
+
+  @Test
+  public void setCipherSuitesThatMayRequireJCEUnlimitedStrengthCrypto() throws Exception {
+    String aes256suite = "TLS_RSA_WITH_AES_256_CBC_SHA256";
+    // This may or may not fail -- depending on the JVM configuration.
+    // This test can go away, I think, once we require Java 8u162 or higher, due to this being fixed: https://bugs.openjdk.java.net/browse/JDK-8170157
+    SSLContext context = SSLContext.getDefault();
+    List<String> supportedCiphers = Arrays.asList(context.getSupportedSSLParameters().getCipherSuites());
+    // This test assumes that the default supported ciphers does *not* include any AES 256 cipher suites.
+    assumeThat(supportedCiphers, not(CoreMatchers.hasItem(aes256suite)));
+
+    try {
+      builder.setCipherSuites(new String[]{aes256suite});
+    } catch (IllegalArgumentException e) {
+      assertThat(e.getMessage(), CoreMatchers.containsString("Java Cryptography Extension"));
+    }
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void setProtocolsWithInvalidProtocol() throws Exception {
+    builder.setProtocols(new String[]{"SSL100"});
+  }
+
+
+}
