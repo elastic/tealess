bf6fc5a11963cdfbfc8144ece21bf87f45eeec62
Jordan Sissel <jls@semicomplete.com>
2017-11-04 21:23:49 -0700

    Check valid cipher is accepted.
    Add a failure to the JCE test if we expect the AES 256 cipher to fail, but it does not fial.

--- src/test/java/co/elastic/tealess/SSLContextBuilderTest.java
+++ src/test/java/co/elastic/tealess/SSLContextBuilderTest.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 import static org.hamcrest.CoreMatchers.not;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
 import static org.junit.Assume.assumeThat;
 
 public class SSLContextBuilderTest {
@@ -21,13 +22,18 @@ public class SSLContextBuilderTest {
 
   @Test
   public void setCipherSuitesWithValidCipherSuite() throws Exception {
+    // This suite should be supported everywhere unless the operator has deployed some custom policy to disable it..
+    builder.setCipherSuites(new String[]{"TLS_RSA_WITH_AES_128_CBC_SHA256"});
   }
 
+  /**
+   * A test to ensure we have a good error message when Oracle's "Unlimited Strength Cryptography" policy is missing and causes AES 256 ciphers to be disabled.
+   * <p>
+   * This test can go away, I think, once we require Java 8u162 or higher, due to this being fixed: https://bugs.openjdk.java.net/browse/JDK-8170157
+   */
   @Test
   public void setCipherSuitesThatMayRequireJCEUnlimitedStrengthCrypto() throws Exception {
     String aes256suite = "TLS_RSA_WITH_AES_256_CBC_SHA256";
-    // This may or may not fail -- depending on the JVM configuration.
-    // This test can go away, I think, once we require Java 8u162 or higher, due to this being fixed: https://bugs.openjdk.java.net/browse/JDK-8170157
     SSLContext context = SSLContext.getDefault();
     List<String> supportedCiphers = Arrays.asList(context.getSupportedSSLParameters().getCipherSuites());
 
@@ -35,6 +41,7 @@ public class SSLContextBuilderTest {
     assumeThat(supportedCiphers, not(CoreMatchers.hasItem(aes256suite)));
     try {
       builder.setCipherSuites(new String[]{aes256suite});
+      fail("setCipherSuites([" + aes256suite + "]) should fail when it is not in the supported ciphers list.");
     } catch (IllegalArgumentException e) {
       assertThat(e.getMessage(), CoreMatchers.containsString("Java Cryptography Extension"));
     }
